b566a45cde6c92b8b13a8382aa89efaab7500737
syedmehedi34 <syedmeehdi34@gmail.com>
2025-03-19 11:59:42 +0600

    controller setup done

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -62,16 +62,16 @@ router.post("/", async (req, res) => {
 // };
 
 // Delete User
-// const deleteUser = async (req, res) => {
-//   try {
-//     const deletedUser = await User.findByIdAndDelete(req.params.id);
-//     if (!deletedUser)
-//       return res.status(404).json({ message: "User not found" });
-//     res.status(200).json({ message: "User deleted successfully" });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
+const deleteUser = async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser)
+      return res.status(404).json({ message: "User not found" });
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 router.module.exports = {
   createUser,
