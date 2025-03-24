4c588f69ae558c08898c6c08f7cdee08de0154a6
MD INZAMUL HAQUE JOY <inzamulhaque1002@gmail.com>
2025-03-24 16:37:06 +0600

    user deleted functiona dded

--- Routes/usersRoutes.js
+++ Routes/usersRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const {
   createUser,
-  getUserByEmail,   getAllUsers,
+  getUserByEmail,   getAllUsers, deleteUser
 
 } = require("../Controllers/usersController");
 
@@ -11,5 +11,6 @@ const router = express.Router();
 router.post("/all_users", createUser); // Create a new user for the consumer
 router.get("/getUser/:email", getUserByEmail);
 router.get("/all_users", getAllUsers); // Get all users
+router.delete("/deleteUser/:id", deleteUser);
 
 module.exports = router;
