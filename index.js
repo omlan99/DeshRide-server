ff51ef996676adb05c1e42c80c2f3eb0e2147ed5
omlan99 <omlan10616@gmail.com>
2025-03-24 17:28:59 +0600

    added start script

--- index.js
+++ index.js
@@ -3,25 +3,19 @@ require("dotenv").config();
 const cors = require("cors");
 const connect = require("./DB");
 
-<<<<<<< HEAD
+const app = express();
 const connectDB = require("./DB")
 connectDB()
-const router = express.Router();
 const users = require("./routes/users");
 
-const app = express()
+
 app.use(express.json())
 
 app.use("/api", users)
-=======
 const userRoutes = require("./Routes/userRoutes"); // Import user routes
->>>>>>> 36bee0850fb46c2f56d36e689f216d0dd6d58021
 
 const port = process.env.PORT || 5001;
-const app = express();
 
-<<<<<<< HEAD
-=======
 // Connect to MongoDB
 connect();
 
@@ -33,7 +27,6 @@ app.use(
     credentials: true, // Allow cookies/credentials to be included in the request
   })
 );
->>>>>>> 36bee0850fb46c2f56d36e689f216d0dd6d58021
 
 // Routes
 app.use("/api/users", userRoutes); // Use the user routes
