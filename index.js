345caf77957c74b10d7e2f4fba88d3f90f779e72
Nahian Mugdho <nahianmugdho@gmail.com>
2025-04-08 22:11:23 +0600

    conflict-solved

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const connect = require("./DB");
+
+const app = express();
+const connectDB = require("./DB")
+connectDB()
+const users = require("./routes/users");
+const cors = require("cors");
+
+
+
+
+app.use("/api", users)
+// const userRoutes = require("./Routes/userRoutes"); // Import user routes
+const usersRoutes = require("./Routes/usersRoutes"); // Import users routes
+// const usersRoutes = require("./Routes/usersRoutes"); // Import users routes
+
+const port = process.env.PORT || 5001;
+
+// Connect to MongoDB
+connect();
+
+// Middleware
+app.use(express.json());
+app.use(
+  cors({
+    // origin: "http://localhost:5173",
+    origin: "",
+
+    credentials: true,
+  })
+);
+
+// Routes
+app.use("/users", usersRoutes); // Use the user routes
+// app.use("/users", providerUserRoutes);
+app.patch("/user/:email" , (req, res) =>{
+  const email = req.params.id
+  const data = req.body
+} )
+
+app.get("/", (req, res) => {
+  res.send("DeshRide Database is connected");
+});
+
+
+// Start Server
+app.listen(port, () => {
+  console.log("App is running on port", port);
+});
